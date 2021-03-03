8abf9ff7a58bc6dc76db701ed0278eb0795663d0
root <root@DESKTOP-5EER1PD.localdomain>
2021-03-03 20:50:52 +0000

    Fixed paused button name

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ prev_button_pic = tk.PhotoImage(file = "images/prev_button.png")
 #Buttons
 prev_button = tk.Button(root, image = prev_button_pic, borderwidth = 0, command = music.play_prev).grid(row=0, column=0)
 play_button = tk.Button(root, image = play_button_pic, borderwidth = 0, command = music.play).grid(row=0, column=1)
-play_button = tk.Button(root, image = pause_button_pic, borderwidth = 0, command = music.pause).grid(row=0, column=2)
+pause_button = tk.Button(root, image = pause_button_pic, borderwidth = 0, command = music.pause).grid(row=0, column=2)
 next_button = tk.Button(root, image = next_button_pic, borderwidth = 0, command = music.play_next).grid(row=0, column=3)
 
 
