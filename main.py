d14af036103fa9273d7fba43bce91ecbc7d44caf
root <root@DESKTOP-5EER1PD.localdomain>
2021-03-03 20:42:25 +0000

    Functionality for play and pause button

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ root = tk.Tk()
 
 #Load images
 play_button_pic = tk.PhotoImage(file = "images/play_button.png")
+pause_button_pic = tk.PhotoImage(file = "images/pause.png") 
 next_button_pic = tk.PhotoImage(file = "images/next_button.png")
 prev_button_pic = tk.PhotoImage(file = "images/prev_button.png")
 
-prev_button = tk.Button(root, image = prev_button_pic, command = music.play_prev()).grid(row=0, column=0)
-play_button = tk.Button(root, image = play_button_pic, command = music.play()).grid(row=0, column=1)
-next_button = tk.Button(root, image = next_button_pic, command = music.play_next()).grid(row=0, column=2)
+#Buttons
+prev_button = tk.Button(root, image = prev_button_pic, borderwidth = 0, command = music.play_prev).grid(row=0, column=0)
+play_button = tk.Button(root, image = play_button_pic, borderwidth = 0, command = music.play).grid(row=0, column=1)
+play_button = tk.Button(root, image = pause_button_pic, borderwidth = 0, command = music.pause).grid(row=0, column=2)
+next_button = tk.Button(root, image = next_button_pic, borderwidth = 0, command = music.play_next).grid(row=0, column=3)
 
 
 root.mainloop()
--- music.py
+++ music.py
@@ -1,11 +1,36 @@
+import pygame
+
 """
 This is a module which will define the algorithms that will be used for the different buttons.
 """
+
+#initialise mixer
+pygame.mixer.init()
+
+global paused
+paused = False
+
 def play():
-    pass
+    global paused
+    if paused:
+        pygame.mixer.music.unpause()
+        paused = False
+    else:
+        pygame.mixer.music.load("songs/Clean Bandit - Symphony (feat. Zara Larsson) [Official Video].mp3")
+        pygame.mixer.music.play(loops=0)
+
+def pause():
+    global paused
+    
+    if paused:
+        pygame.mixer.music.unpause()
+        paused = False   
+    else:
+        pygame.mixer.music.pause()
+        paused = True
 
 def play_next():
     pass
-
+    
 def play_prev():
     pass
