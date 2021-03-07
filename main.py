9754622f29f51ec768befb2e5e6b2640a87d45e4
Ife Makinde <ifemakinde@Ifes-MacBook-Air.local>
2021-03-07 14:52:23 +0000

    Added shuffle button to main.py file

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ play_button_pic = tk.PhotoImage(file = "images/play_button.png")
 pause_button_pic = tk.PhotoImage(file = "images/pause.png") 
 next_button_pic = tk.PhotoImage(file = "images/next_button.png")
 prev_button_pic = tk.PhotoImage(file = "images/prev_button.png")
+shuffle_button_pic = tk.PhotoImage(file = "images/shuffle.png")
 
 #Buttons
 prev_button = tk.Button(root, image = prev_button_pic, borderwidth = 0, command = music.play_prev).grid(row=0, column=0)
 play_button = tk.Button(root, image = play_button_pic, borderwidth = 0, command = music.play).grid(row=0, column=1)
 pause_button = tk.Button(root, image = pause_button_pic, borderwidth = 0, command = music.pause).grid(row=0, column=2)
 next_button = tk.Button(root, image = next_button_pic, borderwidth = 0, command = music.play_next).grid(row=0, column=3)
-
+shuffle_button = tk.Button(root, image = next_button_pic, borderwidth = 0, command = music.play_next).grid(row=0, column=4)
 
 root.mainloop()
