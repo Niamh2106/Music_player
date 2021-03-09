8f661f4b383d7d4a7f97f0570a07071d90ab347d
root <root@DESKTOP-5EER1PD.localdomain>
2021-03-09 12:33:22 +0000

    Fixed Grid Issue

--- music_player_playlist.py
+++ music_player_playlist.py
@@ -25,15 +25,13 @@ def add_song():
 # #Playlist title name
 playlist_name = Label(root, text = "Playlist", bg="red",
 fg = "white",font=("calibri",14,"bold"), width="16")
-playlist_name.pack(side=RIGHT)
-# .grid(row=0, column=1)
+playlist_name.grid(row=0, column=0)
 
 
 #Playlist Box code
 playlist_box = Listbox(root,bg="red",fg="black",font=("calibri"), 
 highlightcolor="darkred", height="20", width="20",)
-playlist_box.pack(side=RIGHT)
-# .grid(row=1, column=1)
+playlist_box.grid(row=1, column=0)
 
 #Create Space
 create_space1 = 1
@@ -41,7 +39,6 @@ create_space1 = 1
 #Add Song Button
 add_songs = Button(root, text = "Add Songs", bg="red",fg="white",font=("calibri",14,
 "bold"), highlightcolor="darkred", width="16",command=add_song)
-add_songs.pack(side=RIGHT)
-# .grid(row=2, column=1)
+add_songs.grid(row=2, column=0)
 
 root.mainloop()
