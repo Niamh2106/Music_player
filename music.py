bdbb4a6faffdd10afff6800cd6015d49cd403a3c
Ife Makinde <ifemakinde@Ifes-MacBook-Air.local>
2021-03-31 16:56:19 +0100

    Removed cache

--- music.py
+++ music.py
@@ -1,14 +1,13 @@
 import pygame
-import random
+
 """
 This is a module which will define the algorithms that will be used for the different buttons.
 """
 
 #initialise mixer
 pygame.mixer.init()
-song_list = []
+
 global paused
-global play_next
 paused = False
 
 def play():
@@ -30,33 +29,8 @@ def pause():
         pygame.mixer.music.pause()
         paused = True
 
-def play_next():
-        i = 0
-
-        while i < len(song_list):
-            pygame.mixer.music.load(song_list[i + 1])
-            pygame.mixer.music.play()
-            if paused:
-            pause()
-            i += 1
-
-def play_prev():
-    i = 0
-
-        while i < len(song_list):
-            if song_list[i] == song_list[0]:
-                song_list[i] = song_list[-1]
-                pygame.mixer.music.load(song_list[i - 1])
-                pygame.mixer.music.play(loops=0)
-            if paused:
-            pause()
-            i += 1
-
-def shuffle():
-    random_song = song_list.choice()
-    pygame.mixer.music.load(random_song)
-    pygame.mixer.music.play(loops=0)
-    if paused:
-            pause()
-
-
+# def play_next():
+#     pass
+    
+# def play_prev():
+#     pass
