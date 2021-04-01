b98b8fac3da922dafc4b5bf732ac2de4a7a88233
Ife Makinde <ifemakinde@Ifes-MacBook-Air.local>
2021-04-01 15:49:19 +0100

    Play Previous And Play Next Button Partially finished

--- music1.py
+++ music1.py
@@ -9,21 +9,26 @@ This is a module which will define the algorithms that will be used for the diff
 """
 #initialise mixer
 pygame.mixer.init()
-
+global songname
+songname = ""
 global path 
 path = ""
 global paused
 paused = False
-
+global song_list
+song_list = []
 #function to add song from file explorer
 def add_song(playlist_box):
     # folder_selected = filedialog.askdirectory()
     song = filedialog.askopenfilename(filetypes=(("mp3 Files", "*.mp3"), ))
+    global songname
     global path
     for songname in song:
         songname = song.split("/")
         path = "/".join(songname)
+        song_list.append(path)
     playlist_box.insert(END,songname[-1])
+    print (songname)
 
 def play():
     global paused
@@ -46,7 +51,25 @@ def pause():
         paused = True
 
 def play_next():
-    pass
+    global paused
+    if paused:
+        pygame.mixer.music.unpause()
+        paused = False
+    else:
+        i = 0
+        while i < len(song_list):
+            pygame.mixer.music.load(song_list[i + 1])
+            pygame.mixer.music.play(loops=0)
+            i += 1
     
 def play_prev():
-    pass
+    global paused
+    if paused:
+        pygame.mixer.music.unpause()
+        paused = False
+    else:
+        i = 0
+        while i < len(song_list):
+            pygame.mixer.music.load(song_list[i])
+            pygame.mixer.music.play(loops=0)
+            i -= 1
