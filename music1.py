587380ef016ee931138e8048601d836a0dc8fc26
Ife Makinde <ifemakinde@Ifes-MacBook-Air.local>
2021-04-02 16:46:02 +0100

    Fixed Play next button to play next song

--- music1.py
+++ music1.py
@@ -4,6 +4,7 @@ import pickle #enable us to get all the metadata about a song
 from tkinter import * #for gui 
 from tkinter import filedialog
 from pygame.constants import BUTTON_RIGHT
+
 """
 This is a module which will define the algorithms that will be used for the different buttons.
 """
@@ -17,6 +18,7 @@ global paused
 paused = False
 global song_list
 song_list = []
+
 #function to add song from file explorer
 def add_song(playlist_box):
     # folder_selected = filedialog.askdirectory()
@@ -26,9 +28,12 @@ def add_song(playlist_box):
     for songname in song:
         songname = song.split("/")
         path = "/".join(songname)
-        song_list.append(path)
+    song_list.append(path)
     playlist_box.insert(END,songname[-1])
-    print (songname)
+    # print (path)
+    # print (len(song_list))
+
+
 
 def play():
     global paused
@@ -36,8 +41,11 @@ def play():
         pygame.mixer.music.unpause()
         paused = False
     else:
-        pygame.mixer.music.load(path)
-        pygame.mixer.music.play(loops=0)
+        i = 0
+        while i < len(song_list):
+            pygame.mixer.music.load(song_list[i])
+            pygame.mixer.music.play(loops=0)
+            i += 1
 
     
 def pause():
@@ -51,17 +59,23 @@ def pause():
         paused = True
 
 def play_next():
-    global paused
-    if paused:
-        pygame.mixer.music.unpause()
-        paused = False
-    else:
-        i = 0
-        while i < len(song_list):
+    i = 0
+    while i < len(song_list):
+        print (i)
+        if len(song_list) == 1:
+            i = 0
+            pygame.mixer.music.load(song_list[i])
+            pygame.mixer.music.play(loops=0)
+
+        elif song_list[i] == song_list[-1]:
+            pygame.mixer.music.load(song_list[0])
+            pygame.mixer.music.play(loops=0)
+        
+        else:
             pygame.mixer.music.load(song_list[i + 1])
             pygame.mixer.music.play(loops=0)
-            i += 1
-    
+        i += 1
+
 def play_prev():
     global paused
     if paused:
