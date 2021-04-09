e097c4d1abfbb058ba33c10b01e56fc9cdc53a8d
root <root@DESKTOP-5EER1PD.localdomain>
2021-04-09 15:19:20 +0100

    V1

--- main.py
+++ main.py
@@ -1,19 +1,88 @@
-import tkinter as tk
+
+from tkinter import * #for gui 
+import pygame
 import music
 
-root = tk.Tk()
+root = Tk()
+root.title('AIM Music Player')
+root.geometry("750x450")
+root.resizable(0, 0)
+root.configure(background='black')
+
+#initialise python mixer/turn on mixer to enable sound
 
 #Load images
-play_button_pic = tk.PhotoImage(file = "images/play_button.png")
-pause_button_pic = tk.PhotoImage(file = "images/pause.png") 
-next_button_pic = tk.PhotoImage(file = "images/next_button.png")
-prev_button_pic = tk.PhotoImage(file = "images/prev_button.png")
+play_button_pic = PhotoImage(file = "images/play_button.png")
+pause_button_pic = PhotoImage(file = "images/pause.png") 
+next_button_pic = PhotoImage(file = "images/next_button.png")
+prev_button_pic = PhotoImage(file = "images/prev_button.png")
+aim_img = PhotoImage(file = "images/aim.png")
+root.iconphoto(False, aim_img)
+
+# Resizing static image
+aim_img = aim_img.subsample(6,6)
+
+# Static Image Frame
+StatImgFrame = LabelFrame(root, bg = "black",bd=0, width=27,pady=0)
+StatImgFrame.grid(row=1, column=0)
+
+#Currently Playing 
+current_play= Label(root, text = "Currently Playing", bg="#990000", fg = "white",font=("calibri",14,), width=27, pady=0, padx=0)
+current_play.grid(row=0,column=0)
+
+#Static Image 
+img = aim_img
+static_image = Label(root, image = img, bg = "#990000")
+static_image.grid(row=1,column=0)
+
+# Playlist Frame
+playlistframe = LabelFrame(root, bg = "black",bd=0, width=27,pady=0)
+playlistframe.grid(row=1, column=4)
+
+#Playlist Label Name
+playlist_name = Label(playlistframe, text = "Playlist", bg="#660000",
+fg = "white",font=("calibri",14,), width =19)
+playlist_name.grid(row=0,column=0)
+
+#Playlist Box code
+playlist_box = Listbox(playlistframe,fg="black",bg = "#990000",font=("calibri",12),width = 23,bd=0,relief="solid",highlightbackground="#660000",highlightthickness=4)
+playlist_box.grid(row=1,column=0)
+
+#Slider Frame
+sliderframe = LabelFrame(root,bg = "black", fg = "black", width = 7, height = 80, bd=0,padx=10,)
+sliderframe.grid(row=1,column=1)
+
+#Slider Label
+slidername = Label(sliderframe, text = "Vol",bg="#990000",font=("calibri",14,),fg="white", width=4,)
+slidername.pack(side=TOP)
+
+#Volume Slider
+volumeslider = Scale(sliderframe,from_=100, to=0, orient=VERTICAL, command= music.volume, fg = "black",bg = "#990000", 
+troughcolor = "black", length=150, relief="solid",highlightbackground="#660000",highlightthickness=2)
+volumeslider.pack(side=BOTTOM)
+volumeslider.set(70)
+
+# Add Song Button
+add_songs = Button(root, text = "Add Song", bg="#990000",fg="white",font=("calibri",14,
+"bold"),width = "16", highlightcolor="darkred",command=lambda: music.add_song(playlist_box), relief="raised")
+add_songs.grid(row=5, column=4)
+
+#Song Title Bar
+SongTitle = Label(root, text = "Song title", bg = "#990000", fg = "white", font=("calibri", 20), highlightbackground="black", width=30, relief="sunken")
+SongTitle.grid(row=4, column=0, padx=10, pady=10)
+
+SongArtist = Label(root, text = "Song artist", bg = "#990000", fg = "white", font=("calibri", 20), highlightbackground="black", width=30, relief="sunken")
+SongArtist.grid(row=5, column=0)
 
 #Buttons
-prev_button = tk.Button(root, image = prev_button_pic, borderwidth = 0, command = music.play_prev).grid(row=0, column=0)
-play_button = tk.Button(root, image = play_button_pic, borderwidth = 0, command = music.play).grid(row=0, column=1)
-pause_button = tk.Button(root, image = pause_button_pic, borderwidth = 0, command = music.pause).grid(row=0, column=2)
-next_button = tk.Button(root, image = next_button_pic, borderwidth = 0, command = music.play_next).grid(row=0, column=3)
+Buttonframe = LabelFrame(root, bg = "black", fg = "black", width = 900, height = 80, bd=0,)
+Buttonframe.grid(row=6, column=0)
+
+
+prev_button = Button(Buttonframe, image = prev_button_pic, bg="black", bd= 4,relief="flat", command = lambda: music.play_prev(SongTitle, SongArtist, static_image)).grid(row=0, column=0,padx=10, pady=10)
+play_button = Button(Buttonframe, image = play_button_pic, bd= 4, bg="black",relief="flat", command = lambda: music.play(SongTitle, SongArtist, static_image)).grid(row=0, column=1,padx=10, pady=10)
+pause_button = Button(Buttonframe, image = pause_button_pic, bd= 4, bg="black",relief="flat", command = music.pause).grid(row=0, column=2, padx=10, pady=10)
+next_button = Button(Buttonframe, image = next_button_pic, bd= 4, bg="black",relief="flat",command = lambda: music.play_next(SongTitle, SongArtist, static_image)).grid(row=0, column=3, padx=10, pady=10)
 
 
 root.mainloop()
--- music.py
+++ music.py
@@ -1,27 +1,116 @@
+"""
+This is a module which will define the algorithms that will be used for the different buttons .
+"""
 import pygame
+from tkinter import filedialog, END, PhotoImage
+import random
+import os
 
-"""
-This is a module which will define the algorithms that will be used for the different buttons.
-"""
+#song class
+class Song:
+    def __init__(self, artist, name, image = ""):
+        self.artist = artist
+        self.name = name
+        self.image = image
 
-#initialise mixer
+    """
+    This is a function that will check if a song has an image associated with it.
+    To associate a song with an image just rename the image to the song name and add the image to the images folder.
+    Example: song name = Paradise then image name = Paradise 
+    """
+    def check_image(self):
+        if os.path.exists("images/" + self.name + ".png"):
+            self.image = PhotoImage(file="images/" + self.name + ".png")
+            self.image = self.image.subsample(3,3)
+        else:
+            self.image = PhotoImage(file="images/aim.png")
+            self.image = self.image.subsample(6,6) 
+
+#initialise the pygame mixer module
 pygame.mixer.init()
+
+global songname
+songname = ""
+
+global path 
+path = ""
 
 global paused
 paused = False
 
-def play():
-    global paused
-    if paused:
-        pygame.mixer.music.unpause()
-        paused = False
-    else:
-        pygame.mixer.music.load("songs/Clean Bandit - Symphony (feat. Zara Larsson) [Official Video].mp3")
-        pygame.mixer.music.play(loops=0)
 
+global i
+i = 0
+
+song_list = []
+songs_objects = {}
+
+#songs must be named in the following format song - artist
+#function to add song from file explorer
+def add_song(playlist_box):
+
+    try:
+        #select song
+        song = filedialog.askopenfilename(filetypes=(("mp3 Files", "*.mp3"), ))
+        path = song
+        
+        #remove .mp3 file extension
+        songname = song.split("/")
+        songname = songname[-1].split(".")
+        songname = songname[:-1]
+        
+        #isolate songname and artist
+        temp = songname
+        songname = ""
+        songname = songname.join(temp)
+        songname = songname.split("-")
+
+        #create song object
+        song_obj = Song(songname[0].strip(), songname[1].strip())
+        song_obj.check_image()
+        songs_objects[song_obj] = path
+
+        #add song to playlist
+        song_list.append(path)
+        playlist_box.insert(END,song_obj.artist + song_obj.name)
+    except IndexError:
+        pass
+
+#play button
+def play(name, artist, image):
+    try:
+        global paused
+
+        #check if already paused
+        if paused:
+            pygame.mixer.music.unpause()
+            paused = False   
+        else:
+            obj = None
+
+            #obtain songname
+            for key, value in songs_objects.items():
+                if value == song_list[i]:
+                    obj = key
+                    break
+
+            #Display current song
+            name.config(text=obj.name)
+            artist.config(text=obj.artist)
+            image.config(image=obj.image, bg=None)
+
+            #load and play song
+            pygame.mixer.music.load(song_list[i])
+            pygame.mixer.music.play(loops=0)
+    except AttributeError:
+        pass
+
+
+#pause button  
 def pause():
     global paused
     
+    #check if already paused
     if paused:
         pygame.mixer.music.unpause()
         paused = False   
@@ -29,8 +118,66 @@ def pause():
         pygame.mixer.music.pause()
         paused = True
 
-def play_next():
-    pass
-    
-def play_prev():
-    pass
+#play next button
+def play_next(name, artist, image):
+    try:
+        global i
+
+        #check if current song is last song
+        if i == len(song_list) - 1:
+            i = -1
+            pygame.mixer.music.load(song_list[i + 1])
+            pygame.mixer.music.play(loops=0)
+            i += 1
+        else:
+            pygame.mixer.music.load(song_list[i + 1])
+            pygame.mixer.music.play(loops=0)
+            i += 1
+        
+        obj = None
+        #obtain songname
+        for key, value in songs_objects.items():
+            if value == song_list[i]:
+                obj = key
+                break
+
+        #Display current song
+        name.config(text=obj.name)
+        artist.config(text=obj.artist)
+        image.config(image=obj.image, bg=None)
+    except IndexError:
+        pass
+
+#play previous button
+def play_prev(name, artist, image):
+    try:
+        global i
+
+        #check if current song is first song
+        if i == 0:
+            i = len(song_list) - 1
+            pygame.mixer.music.load(song_list[i])
+            pygame.mixer.music.play(loops=0)
+        else:
+            pygame.mixer.music.load(song_list[i - 1])
+            pygame.mixer.music.play(loops=0)
+            i -= 1
+        
+        obj = None
+        #obtain songname
+        for key, value in songs_objects.items():
+            if value == song_list[i]:
+                obj = key
+                break
+
+        #Display current song
+        name.config(text=obj.name)
+        artist.config(text=obj.artist)
+        image.config(image=obj.image, bg=None)
+    except IndexError:
+        pass
+
+#volume slider
+def volume(val):
+    volume = int(val)
+    pygame.mixer.music.set_volume(volume)
